539c1818c76dfd8250d057a8e27e6513e2f7deaa
Guilherme Faga <faga@retize.com.br>
2023-09-10 22:00:59 -0300

    feat(#1): admin user tests

--- rules/tests/users.test.js
+++ rules/tests/users.test.js
@@ -81,3 +81,57 @@ describe('Normal user', () => {
 })
 
 
+describe('Admin user', () => {
+  test('should get its own data', async () => {
+    expect.assertions(1);
+
+    const { adminUser } = vars;
+
+    const db = adminUser.firestore();
+    const user = await getDoc(doc(db, `usuarios/${adminUser.authToken.user_id}`));
+    const userData = user.data();
+
+    expect(clearObject(userData)).toEqual(clearObject(schemas.usuarios.usuario));
+  });
+
+
+  test('should not write its own data', async () => {
+    expect.assertions(1);
+
+    const { adminUser } = vars;
+
+    const db = adminUser.firestore();
+    try {
+      await setDoc(doc(db, `usuarios/${adminUser.authToken.user_id}`), {});
+    } catch (error) {
+      expect(error).toBeTruthy();
+    }
+  });
+
+
+  test('should not read other user data', async () => {
+    expect.assertions(1);
+
+    const { adminUser, normalUser } = vars;
+
+    const db = adminUser.firestore();
+    try {
+      await getDoc(doc(db, `usuarios/${normalUser.authToken.user_id}`));
+    } catch (error) {
+      expect(error).toBeTruthy();
+    }
+  });
+
+  test('should not write other user data', async () => {
+    expect.assertions(1);
+
+    const { adminUser, normalUser } = vars;
+
+    const db = adminUser.firestore();
+    try {
+      await setDoc(doc(db, `usuarios/${normalUser.authToken.user_id}`), {});
+    } catch (error) {
+      expect(error).toBeTruthy();
+    }
+  });
+})
